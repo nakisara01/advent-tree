f28675552d1917f1651e0ca63e80626af775d784
joice_c <hyunjin.kim@crytoparadise.io>
2022-12-03 15:18:02 +0900

    fix:reserve conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
-import "./App.css";
-import SojuTree from "./component/SojuTree.js";
-import NavBar from "./component/NavBar.js";
+import './App.css';
+import SojuTree from './component/SojuTree.js';
+import NavBar from './component/NavBar.js';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Auth from './hoc/auth';
+
+/* Components */
+import MainPage from './components/MainPage';
+import LoginPage from './components/LoginPage';
 
 function App() {
+  // const AuthMainPage = Auth(MainPage, false);
+  // const AuthLoginPage = Auth(LoginPage, false);
   return (
     <div className="App">
-      <NavBar />
-      <SojuTree />
+      <Router>
+        <Routes>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/login" component={LoginPage} />
+          {/* <Route exact path="/" element={<AuthMainPage />} />
+        <Route exact path="/login" element={<AuthLoginPage />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
